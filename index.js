698ccb8d407e6d08cce7318d557fc78aa791a0ef
muhsalaa <muhsalaa@gmail.com>
2020-10-21 11:43:40 +0700

    add docs

--- index.js
+++ index.js
@@ -33,14 +33,15 @@ reportWeekly(bot);
 
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', process.env.PORT || 5000);
+app.use(express.static(__dirname + '/public'));
 
 //For avoidong Heroku $PORT error
 app
   .get('/', function (request, response) {
-    var result = 'App is running';
-    response.send(result);
+    response.sendFile(path.join(__dirname + 'index.html'));
   })
   .listen(app.get('port'), function () {
     console.log(
